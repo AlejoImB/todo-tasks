ced85706effccadc60172891b690878a929f8b89
Alejandro Imbachi Escalante <alejoimb10@outlook.com>
2024-08-12 16:34:04 -0500

    task schema, create task

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 var express = require("express");
 var router = express.Router();
+var TaskModel = require('./task_schema');
 
 if (!process.env.ON_RENDER) {
     console.log("Cargando variables de entorno desde archivo");
@@ -30,4 +31,26 @@ mongoose.connect(query, {
     }
 });
 
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+    var newTask = new TaskModel(task);
+
+    newTask.save(function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
 module.exports = router;
